a82fff925141f779445d9514af0b140a7d7f6cdd
scsi <scsichen@gmail.com>
2015-04-11 17:02:04 +0800

    fix

--- profile/bin/checkupdate.sh
+++ profile/bin/checkupdate.sh
@@ -64,6 +64,7 @@ checkit()
 
 	SUBWORKDIR=$WORKDIR/$NAME
 	RAWDATA=$SUBWORKDIR/raw.html
+	RAWTEXT=$SUBWORKDIR/raw.txt
 	NOWLIST=$SUBWORKDIR/now.list
 	ORIGLIST=$SUBWORKDIR/orig.list
 	DIFFDATA=$SUBWORKDIR/diff.list
@@ -72,18 +73,19 @@ checkit()
 
 	if [ ! -f $NOWLIST -o "$action" != reset ]
 	then
-		startprogress
+		#startprogress
 		if wget --no-cache --timeout="$TIMEOUT" -t "$RETRY" -q -O - $URL>$RAWDATA 2>/dev/null
 		then
+			cat $RAWDATA|sed 's/width/widta/'|w3m -O utf-8 -dump -T  text/html -cols 1000 >$RAWTEXT
 			if [ -n "$PATTERN" ]
 			then
-				cat $RAWDATA|sed 's/width/widta/'|w3m -O big5 -dump -T  text/html -cols 1000|grep -e "$PATTERN" |tr -s " ">$NOWLIST
+				cat $RAWTEXT|(eval $PATTERN)|tr -s " ">$NOWLIST
 			else
-				cat $RAWDATA|sed 's/width/widta/'|w3m -O big5 -dump -T  text/html -cols 1000 |tr -s " ">$NOWLIST
+				cat $RAWTEXT|tr -s " ">$NOWLIST
 			fi
-			stopprogress
+			#stopprogress
 		else
-			stopprogress
+			#stopprogress
 			echo "Get information fail."
 			return
 		fi
@@ -100,8 +102,9 @@ checkit()
 			echo "Reset data."
 		fi
 	else	
-		[ -f $ORIGLIST ]  || cp $NOWLIST $ORIGLIST
-		if diff -Nu $ORIGLIST $NOWLIST >$DIFFDATA
+		[ -f $ORIGLIST ] || touch $ORIGLIST
+		diff -Nu $ORIGLIST $NOWLIST|sed -n 4,\$p >$DIFFDATA
+		if [ ${PIPESTATUS[0]} -eq 0 ]
 		then
 			echo "Up to date."
 		else
@@ -111,50 +114,55 @@ checkit()
 				printf "subject: Data update - ${NAME}\n\n`cat $DIFFDATA`\r\n"|/usr/lib/sendmail $MAILADDR
 			else
 				echo "Update available"
-				echo "================================="
+				echo "-"
 				cat $DIFFDATA
-				echo "================================="
+				echo 
 			fi
 		fi
 	fi
 }
 
-add_data "GPS9900" "http://www.tw-radar.com.tw/tech/file/upfile_list.asp" "GPS.*9900.*•x∆W"
-add_data "GPS9968" "http://www.5685.com.tw/default.php" "•x∆W™©"
-add_data "Mobile01" "http://www.mobile01.com/downloads.php" "ßÛ∑s§È"
-add_data "Garmin350" "http://www.garmin.com.tw/products/Nuvi350_TWN/download.htm" ""
-add_data "Garmin" "http://www.garmin.com.tw/support/download.htm" "\(MapSource\|Nuvi 350\|USB\).*[0-9][0-9]/[0-9][0-9]/[0-9][0-9]"
-add_data "Gcin" "http://cle.linux.org.tw/gcin/download" "gcin.*\.tar\."
-add_data "PCManX" "http://pcmanx.csie.net" "pcmanx-gtk2.*\.tar\."
-add_data "SMPlayer" "http://kde-apps.org/content/show.php/SMPlayer?content=54487" "Version:"
-add_data "smplayer-theme" "http://smplayer.sourceforge.net/downloads.php?tr_lang=en" "smplayer-.*\.tar\."
-add_data "KDMTheme" "http://kde-apps.org/content/show.php/KDM+Theme+Manager?content=22120" "Version:"
-add_data "KSquirrel" "http://kde-apps.org/content/show.php/KSquirrel?content=12317" "Version:"
-add_data "Yakuake" "http://kde-apps.org/content/show.php/Yakuake?content=29153" "Version:"
-add_data "KSniffer" "http://kde-apps.org/content/show.php/KSniffer?content=26258" "Version:"
-add_data "QStarDict" "http://qstardict.ylsoftware.com" "latest version"
-add_data "KGtk" "http://kde-apps.org/content/show.php/KGtk+%28Use+KDE+Dialogs+in+Gtk+Apps%29?content=36077" "Version:"
-add_data "GPicView" "http://sourceforge.net/project/showfiles.php?group_id=180858" "Latest"
-add_data "Stardict" "http://stardict.sourceforge.net/other.php" "Source code package"
-add_data "Sdcv" "http://stardict.sourceforge.net/other.php" "Command line version"
-add_data "scim-array" "http://scimarray.openfoundry.org/" "•ÿ´e≥Ã∑s™©•ª"
-add_data "scim-chewing" "http://chewing.csie.net/download/scim/" "scim-chewing"
-add_data "libchewing" "http://chewing.csie.net/download/libchewing/" "libchewing"
-add_data "pcmanfm" "http://sourceforge.net/project/showfiles.php?group_id=156956" "Latest"
-add_data "antcontrib" "http://sourceforge.net/projects/ant-contrib/files/ant-contrib" "Latest"
-add_data "gtkhirad" "http://pcman.sayya.org/gtkhirad" "gtkhirad"
-add_data "kalsamix" "http://sourceforge.net/project/showfiles.php?group_id=194230" "Latest"
-add_data "autostart" "http://beta.smileaf.org/files/autostart" "autostart"
-add_data "kwlan" "http://home.arcor.de/tom.michel/downloads.html" "Version"
-add_data "kwirelessmonitor" "http://www.cs.cmu.edu/~pach/kwirelessmonitor/" "Source"
-add_data "wlassistant" "http://sourceforge.net/project/showfiles.php?group_id=134488" "Latest"
-add_data "hinedo" "http://rt.openfoundry.org/Foundry/Project/?Queue=814" "New release"
-add_data "Kirocker" "http://www.kde-apps.org/content/show.php?content=52869" "Version:"
-add_data "imageshack-upload" "http://kde-apps.org/content/show.php?content=51247" "Version:"
-add_data "kconnections" "http://kde-apps.org/content/show.php?content=71204" "Version:"
-add_data "CDFS" "http://trappist.elis.ugent.be/~mronsse/cdfs/download/" "cdfs.*"
-add_data "Gmail-Notify" "http://sourceforge.net/project/showfiles.php?group_id=125937" "Latest"
-add_data "KGmail-Notify" "http://www.kde-apps.org/content/show.php/KGmailNotifier?content=55375" "Version:"
+add_data "XR-3008" "http://conqueror.gpscamera.org/" "grep 'Âè∞ÁÅ£ÁõÆÂâçÊúÄÊñ∞ÁâàÊú¨'"
+add_data "Redmine" "http://www.redmine.org/projects/redmine/wiki/Download" "awk '/Latest releases/,/Resources/'|grep -v -e : -e '^$'"
+add_data "Eclipse" "https://eclipse.org/downloads/" "grep 'Release for'"
+add_data "Jenkins" "https://jenkins-ci.org/" "grep 'Latest and greatest'"
+
+#add_data "GPS9900" "http://www.tw-radar.com.tw/tech/file/upfile_list.asp" "GPS.*9900.*?x?W"
+#add_data "GPS9968" "http://www.5685.com.tw/default.php" "?x?W??"
+#add_data "Mobile01" "http://www.mobile01.com/downloads.php" "???s??"
+#add_data "Garmin350" "http://www.garmin.com.tw/products/Nuvi350_TWN/download.htm" ""
+#add_data "Garmin" "http://www.garmin.com.tw/support/download.htm" "\(MapSource\|Nuvi 350\|USB\).*[0-9][0-9]/[0-9][0-9]/[0-9][0-9]"
+#add_data "Gcin" "http://cle.linux.org.tw/gcin/download" "gcin.*\.tar\."
+#add_data "PCManX" "http://pcmanx.csie.net" "pcmanx-gtk2.*\.tar\."
+#add_data "SMPlayer" "http://kde-apps.org/content/show.php/SMPlayer?content=54487" "Version:"
+#add_data "smplayer-theme" "http://smplayer.sourceforge.net/downloads.php?tr_lang=en" "smplayer-.*\.tar\."
+#add_data "KDMTheme" "http://kde-apps.org/content/show.php/KDM+Theme+Manager?content=22120" "Version:"
+#add_data "KSquirrel" "http://kde-apps.org/content/show.php/KSquirrel?content=12317" "Version:"
+#add_data "Yakuake" "http://kde-apps.org/content/show.php/Yakuake?content=29153" "Version:"
+#add_data "KSniffer" "http://kde-apps.org/content/show.php/KSniffer?content=26258" "Version:"
+#add_data "QStarDict" "http://qstardict.ylsoftware.com" "latest version"
+#add_data "KGtk" "http://kde-apps.org/content/show.php/KGtk+%28Use+KDE+Dialogs+in+Gtk+Apps%29?content=36077" "Version:"
+#add_data "GPicView" "http://sourceforge.net/project/showfiles.php?group_id=180858" "Latest"
+#add_data "Stardict" "http://stardict.sourceforge.net/other.php" "Source code package"
+#add_data "Sdcv" "http://stardict.sourceforge.net/other.php" "Command line version"
+#add_data "scim-array" "http://scimarray.openfoundry.org/" "?ÿ´e?Ã∑s????"
+#add_data "scim-chewing" "http://chewing.csie.net/download/scim/" "scim-chewing"
+#add_data "libchewing" "http://chewing.csie.net/download/libchewing/" "libchewing"
+#add_data "pcmanfm" "http://sourceforge.net/project/showfiles.php?group_id=156956" "Latest"
+#add_data "antcontrib" "http://sourceforge.net/projects/ant-contrib/files/ant-contrib" "Latest"
+#add_data "gtkhirad" "http://pcman.sayya.org/gtkhirad" "gtkhirad"
+#add_data "kalsamix" "http://sourceforge.net/project/showfiles.php?group_id=194230" "Latest"
+#add_data "autostart" "http://beta.smileaf.org/files/autostart" "autostart"
+#add_data "kwlan" "http://home.arcor.de/tom.michel/downloads.html" "Version"
+#add_data "kwirelessmonitor" "http://www.cs.cmu.edu/~pach/kwirelessmonitor/" "Source"
+#add_data "wlassistant" "http://sourceforge.net/project/showfiles.php?group_id=134488" "Latest"
+#add_data "hinedo" "http://rt.openfoundry.org/Foundry/Project/?Queue=814" "New release"
+#add_data "Kirocker" "http://www.kde-apps.org/content/show.php?content=52869" "Version:"
+#add_data "imageshack-upload" "http://kde-apps.org/content/show.php?content=51247" "Version:"
+#add_data "kconnections" "http://kde-apps.org/content/show.php?content=71204" "Version:"
+#add_data "CDFS" "http://trappist.elis.ugent.be/~mronsse/cdfs/download/" "cdfs.*"
+#add_data "Gmail-Notify" "http://sourceforge.net/project/showfiles.php?group_id=125937" "Latest"
+#add_data "KGmail-Notify" "http://www.kde-apps.org/content/show.php/KGmailNotifier?content=55375" "Version:"
 
 set -- `getopt mM: $*||usage` 
 for i
