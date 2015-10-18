15bc557d2942bc731e89df8d2f5d75e525bc4a94
scsi <scsichen@gmail.com>
2015-10-18 17:23:07 +0800

    fix

--- profile/gen_kde_keyworlds.sh
+++ profile/gen_kde_keyworlds.sh
@@ -1,3 +1,3 @@
-(cd /usr/portage/official/;find dev-qt kde-base kde-apps -maxdepth 1 -mindepth 1 -type d|sed "s/$/:4/") \
+(cd /usr/portage/official/;find dev-qt kde-base kde-apps -maxdepth 1 -mindepth 1 -type d|sed -e "s/^/<=/" -e "s/$/-5/") \
 |tee package.keywords/kde.keywords
 
