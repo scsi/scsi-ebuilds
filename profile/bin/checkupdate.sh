509872e27e7951b9e46070fd1806081354c15354
scsi <scsichen@gmail.com>
2015-11-23 17:57:39 +0800

    ifx

--- profile/bin/checkupdate.sh
+++ profile/bin/checkupdate.sh
@@ -116,6 +116,8 @@ add_data "Redmine" "http://www.redmine.org/projects/redmine/wiki/Download" "awk 
 add_data "Eclipse" "https://eclipse.org/downloads/" "grep 'Release for'"
 add_data "Jenkins" "https://jenkins-ci.org/" "grep 'Latest and greatest'"
 #add_data "Docker" "https://docs.docker.com/release-notes/" "grep 'Release Notes Version'"
+
+add_data "docker-registry" "https://github.com/docker/distribution/releases" "grep '• v'"
 add_data "Slf4j" "http://www.slf4j.org/download.html" "grep '.zip$'"
 add_data "Logback" "http://logback.qos.ch/download.html" "grep '.zip$'"
 add_data "Log4j" "http://logging.apache.org/log4j/2.x/download.html" "grep 'Apache Log4j 2 binary (zip)'"
