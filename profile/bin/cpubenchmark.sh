12ff547c5c62a80891b7cbe30fa91e6927c5bb1e
scsi <scsichen@gmail.com>
2016-01-20 22:36:21 +0800

    fix

--- profile/bin/cpubenchmark.sh
+++ profile/bin/cpubenchmark.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 #http://www.linuxidc.com/Linux/2009-06/20495.htm
-time echo 'scale=5000; 4*a(1)' | bc -l -q
-echo cpu cores:`nproc`
+C=10000000000
+stime=`date +%s%3N`
+echo 'scale=5000; 4*a(1)' | bc -l -q >/dev/null
+etime=`date +%s%3N`
+((goal=C/(etime-stime)))
+nc=`nproc`
+echo CPU: $goal \* $nc = $((goal*nc))
