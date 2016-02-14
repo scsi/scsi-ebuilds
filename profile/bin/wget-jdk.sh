45d0083a64ea68d33eb62f375bacc03360610f77
scsi <scsichen@gmail.com>
2016-02-14 15:22:52 +0800

    fix

--- profile/bin/wget-jdk.sh
+++ profile/bin/wget-jdk.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #http://stackoverflow.com/questions/10268583/downloading-java-jdk-on-linux-via-wget-is-shown-license-page-instead
 #http://download.oracle.com/otn-pub/java/jdk/8u74-b02/jdk-8u74-linux-x64.tar.gz
-wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" $1
+wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" "$@"
