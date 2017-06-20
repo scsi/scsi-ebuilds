711700151739b0f5ccc9c388fc24acb8868bdc2f
scsi <scsichen@gmail.com>
2017-06-20 08:16:53 +0800

    update conver table

--- sys-libs/glibc/files/update.sh
+++ sys-libs/glibc/files/update.sh
@@ -1,3 +1,3 @@
 
-rsync -tDhv --chmod=go=r  srissrv:/home/scsi/work/sris-tool-svn/AE-CNS/table/result/{EUC-TW,euc2ucs.h,ucs2euc.h} .
+rsync -tDhv --chmod=go=r  srissrv:/home/sriscm/FONT_SERVER/REPOS/table/result/{EUC-TW,euc2ucs.h,ucs2euc.h} .
 
