5a06dfc22ac1a1cc8c92867176aeea8958591600
scsi <scsichen@gmail.com>
2019-07-31 13:53:31 +0800

    fix

--- profile/bin/clear_fs_cache.sh
+++ profile/bin/clear_fs_cache.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 #https://unix.stackexchange.com/questions/253816/restrict-size-of-buffer-cache-in-linux
+# 1: 
+#
+# sudo sysctl vm.drop_caches=3
 echo 3 > /proc/sys/vm/drop_caches
+
+#vm.min_free_kbytes looks like it could help. Eg. executing
+#echo 5248000 > /proc/sys/vm/min_free_kbytes
