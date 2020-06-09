7f8ac53dea27117d02c8a3706a33e257b8211951
scsi <scsichen@gmail.com>
2020-06-10 04:29:48 +0800

    fix

--- profile/gen_kde5_keyworlds.sh
+++ profile/gen_kde5_keyworlds.sh
@@ -3,5 +3,4 @@
 #(cd /usr/portage/official/;find dev-qt kde-frameworks kde-plasma kde-apps -maxdepth 1 -mindepth 1 -type d|sed -e "s/^/>=/" -e "s/$/-5/") \
 
 #(cd /usr/portage/official/;find dev-qt kde-frameworks kde-plasma kde-apps -maxdepth 1 -mindepth 1 -type d) \
-(cd /usr/portage/official/;find kde-frameworks kde-plasma kde-apps -maxdepth 1 -mindepth 1 -type d) \
-|sort|tee package.keywords/kde-frameworks.keywords
+(cd /usr/portage/official/;find kde-frameworks kde-plasma kde-apps -maxdepth 1 -mindepth 1 -type d)|sort|tee package.accept_keywords/kde-frameworks.accept_keywords
